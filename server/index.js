ebacdcfa41057106d9e5353cbe44683e95f72d41
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2023-08-03 23:20:09 +0530

    Port in index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT ||  8080;
 app.use(cors());
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
